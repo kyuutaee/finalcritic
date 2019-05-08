a822c1885487a1d24944f94ba7a6a53185cf08d8
kyuutaee <46968929+kyuutaee@users.noreply.github.com>
2019-05-08 11:05:53 -0400

    Add files via upload

---
+++ FinalCritic/p5/sketch.js
@@ -0,0 +1,23 @@
+
+let song;
+
+function preload() {
+  song = loadSound('focus.ogg');
+}
+
+function setup() {
+  createCanvas(710, 200);
+  song.loop(); // song is ready to play during setup() because it was loaded during preload
+  background(0, 255, 0);
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.pause(); // .play() will resume from .pause() position
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
+  }
+}
